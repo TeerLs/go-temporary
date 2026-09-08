e1c2a9ce5624accc8f5c4b937847687eac62492a
Pavel Rudenko <rudenkopavel21@gmail.com>
2026-09-08 16:01:59 +0300

    Fixed

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"temporary/config"
 	"temporary/internal/product"
 	"temporary/pkg/db"
 	"temporary/pkg/middleware"
+
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+  // Log as JSON instead of the default ASCII formatter.
+  log.SetFormatter(&log.JSONFormatter{})
+
+  // Output to stdout instead of the default stderr
+  // Can be any io.Writer, see below for File example
+  log.SetOutput(os.Stdout)
+
+  // Only log the warning severity or above.
+  log.SetLevel(log.WarnLevel)
+}
 
 func main() {
 	cfg := config.Config{}
--- internal/product/payload.go
+++ internal/product/payload.go
@@ -3,9 +3,11 @@ package product
 type CreateProductRequest struct {
 	Name  string  `json:"name" validate:"required"`
 	Description string  `json:"description" validate:"required"`
+	Images      []string `json:"images" validate:"required"`
 }
 
 type UpdateProductRequest struct {
 	Name        string `json:"name" validate:"required"`
 	Description string `json:"description"`
+	Images      []string `json:"images" validate:"required"`
 }
